e281dad5cfe3267e319142a9ee9903d2317a060b
Shubham-98k <tech.shubhampal@gmail.com>
2026-04-24 21:01:09 +0530

    Implemented. get user by id api route

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -1,5 +1,6 @@
 import Task from "../models/task.model.js"
 import User from "../models/user.model.js"
+import { errorhandler } from "../utils/error.js"
 
 export const getUsers = async (req, res, next) =>{
     try{
@@ -35,6 +36,21 @@ export const getUsers = async (req, res, next) =>{
 
         res.status(200).json(usersWithTaskCounts)
     } catch(error){
+        next(error)
+    }
+}
+
+export const getUserById = async (req,res,next)=>{
+    try{
+        const user = await User.findById(req.params.id).select("-password")
+
+        if(!user){
+            return next(errorhandler(404,"User not found"))
+        }
+        
+        res.status(200).json(user)
+
+    }catch(error){
         next(error)
     }
 }
