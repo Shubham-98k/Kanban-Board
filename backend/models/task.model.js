0ffc2bb0f814e061b773b1ab40565f9f6405eaf2
Shubham-98k <tech.shubhampal@gmail.com>
2026-04-24 21:02:43 +0530

    Added todoSchema model

--- backend/models/task.model.js
+++ backend/models/task.model.js
@@ -1,6 +1,20 @@
 import mongoose from "mongoose";
 
+const todoSchema = new mongoose.Schema({
+        text:{
+            type: String,
+            required:true,
+        },
+
+        completed:{
+            type: Boolean,
+            default: false,
+        }
+
+    })
+
 const taskSchema = new mongoose.Schema({
+
     title:{
         type: String,
         required: true,
